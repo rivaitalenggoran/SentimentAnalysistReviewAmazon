637e6f1a4b69800d77b4ad488cb0ef8b7af6cacd
rivaitalenggoran <rivaitalenggoran9@gmail.com>
2024-08-07 15:49:19 +0700

    Upload Projek

--- EksplorasiData.py
+++ EksplorasiData.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+
+
 file = "Dataset//Electronics_sampled.csv"
-
 data = pd.read_csv(file)
-
 
 
 #Distribusi Rating-----------------------------------------------------------
@@ -39,7 +39,7 @@ data['timestamp'] = pd.to_datetime(data['timestamp'])
 data.set_index('timestamp', inplace=True)
 
 # Mengelompokkan data berdasarkan bulan dan tahun, kemudian menghitung rata-rata rating per bulan
-monthly_ratings = data['rating'].resample('M').mean()
+monthly_ratings = data['rating'].resample('ME').mean()
 
 # Membuat grafik waktu seri
 plt.figure(figsize=(12, 6))
@@ -55,26 +55,74 @@ plt.grid(True)
 plt.show()
 
 
-
-#Analisa Judul Review Yang Sering Muncul------------------------------------------
+#Analisa Teks Judul Review Yang Sering Muncul------------------------------------------
 text = ' '.join(data['title'].dropna())
 wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
-
 plt.figure(figsize=(12, 6))
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis('off')
-plt.title('Word Cloud dari Judul Ulasan')
+plt.title('Judul Review Yang Sering Muncul')
 plt.show()
 
 
 
-#Analisa Isi Review Yang Sering Muncul------------------------------------------
+#Analisa Teks Di Review Yang Sering Muncul------------------------------------------
 text = ' '.join(data['text'].dropna())
 wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
-
 plt.figure(figsize=(12, 6))
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis('off')
-plt.title('Word Cloud dari Teks Ulasan')
+plt.title('Teks Review Yang Sering Muncul')
 plt.show()
 
+
+
+#Jumlah Gambar Per Ulasan---------------------------------------------------------
+data['num_images'] = data['images'].apply(lambda x: len(eval(x)) if pd.notna(x) else 0)
+plt.figure(figsize=(10, 6))
+data['num_images'].hist(bins=range(0, data['num_images'].max() + 1), color='lightgreen')
+plt.title('Distribusi Jumlah Gambar per Ulasan')
+plt.xlabel('Jumlah Gambar')
+plt.ylabel('Jumlah Ulasan')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+
+
+#Frekuensi Kode Produk Yang Muncul-----------------------------------------------
+plt.figure(figsize=(12, 6))
+data['asin'].value_counts().head(10).plot(kind='bar', color='salmon')
+plt.title('10 ASIN Teratas berdasarkan Jumlah Ulasan')
+plt.xlabel('ASIN')
+plt.ylabel('Jumlah Ulasan')
+plt.xticks(rotation=90)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+
+#Frekuensi Ulasan User----------------------------------------------------------
+plt.figure(figsize=(12, 6))
+data['user_id'].value_counts().head(10).plot(kind='bar', color='orange')
+plt.title('10 User ID Teratas berdasarkan Jumlah Ulasan')
+plt.xlabel('User ID')
+plt.ylabel('Jumlah Ulasan')
+plt.xticks(rotation=90)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+#Frekuensi Review Yang Bermanfaat----------------------------------------------
+plt.figure(figsize=(10, 6))
+data['helpful_vote'].hist(bins=range(0, data['helpful_vote'].max() + 1), color='purple')
+plt.title('Distribusi Jumlah Vote Berguna per Ulasan')
+plt.xlabel('Jumlah Vote Berguna')
+plt.ylabel('Jumlah Ulasan')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+
+#Distribusi Pembelian Yang Terverifikasi-------------------------------------
+plt.figure(figsize=(6, 6))
+data['verified_purchase'].value_counts().plot(kind='pie', autopct='%1.1f%%', colors=['lightblue', 'lightcoral'])
+plt.title('Distribusi Ulasan Pembelian Terverifikasi')
+plt.ylabel('')
+plt.show()
--- PraProcessing(Sampling).py
+++ PraProcessing(Sampling).py
@@ -1,12 +1,46 @@
 import pandas as pd
+import regex as re
 
+
+#persiapkan data
 input_file = 'Dataset//Electronics.jsonl'
 output_file = 'Dataset//Electronics_sampled.csv'
 num_objects = 10000
 
 
-# Membaca file JSONL dan mengambil 1000 objek pertama
+
+# Membaca file JSONL dan mengambil 10000 objek pertama
 data = pd.read_json(input_file, lines=True, nrows=num_objects)
 
+
+
+#LowerCase title, Menghapus karakter tidak penting
+title_data = data['title']
+new_title_data = []
+for title in title_data:
+    lower_title = title.lower()
+    new_title = re.sub(r"[..!|?|&|,|-|/|\|:|’|'|(|)|%|#|$|..]", '', lower_title)
+    new_title_data.append(new_title)
+
+
+
+#regex untuk hapus <br /> pada review, LowerCase, Menghapus karakter tidak penting
+text_data = data['text']
+new_text_data = []
+for text in text_data:
+    new_text = re.sub(r"<br\s*/?>", '', text)
+    lower_text = new_text.lower()
+    new_text = re.sub(r"[..!|?|&|,|-|/|\|:|’|'|(|)|%|#|$|..]", '', lower_text)
+    new_text_data.append(new_text)
+
+
+new_title_df = pd.Series(new_title_data)
+data['title'] = new_title_df
+new_text_df = pd.Series(new_text_data)
+data['text'] = new_text_df
+
+
+concat_df = pd.concat([data['rating'],data['title'],data['text'],data['images'],data['asin'],data['parent_asin'],data['user_id'],data['timestamp'],data['helpful_vote'],data['verified_purchase']],axis=1)
+new_df = pd.DataFrame(concat_df)
 # Menyimpan data dalam format CSV
-data.to_csv(output_file, index=False, encoding='utf-8')
+new_df.to_csv(output_file, index=False, encoding='utf-8')

ae57093c725c9a6811e4fba8471912e22991bcd9
rivaitalenggoran <rivaitalenggoran9@gmail.com>
2024-08-08 16:48:15 +0700

    Upload Projek #3

--- PraProcessing.py
+++ PraProcessing.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import regex as re
-from textblob import TextBlob
 import string
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
@@ -13,7 +12,7 @@ num_objects = 10000
 
 
 
-# Membaca file JSONL dan mengambil 10000 objek pertama
+# Membaca file JSONL
 data = pd.read_json(input_file, lines=True, nrows=num_objects)
 
 #Set StopWord dari NLTK
@@ -21,6 +20,7 @@ stop_words = set(stopwords.words('english'))
 
 # Inisialisasi WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
+
 
 '''
 Praprocessing :
@@ -41,21 +41,21 @@ for title in title_data:
     number_title = re.sub(r'\d+', '', lower_title)
     #hapus karakter
     character_title = number_title.translate(str.maketrans('', '', string.punctuation))
-    #Tokenisasi
+    #tokenisasi
     token_title = character_title.split()
     #hapus Stopword
     stopword_title = [word for word in token_title if word not in stop_words]
-    #Lemmatisasi
+    #lemmatisasi
     lematisasi_title = [lemmatizer.lemmatize(stopwords) for stopwords in stopword_title]
     new_title_data.append(lematisasi_title)
 
 
 
-#regex untuk hapus <br /> pada review, LowerCase, Menghapus karakter tidak penting
+#regex untuk hapus <br /> pada review, lowerCase, menghapus karakter tidak penting
 text_data = data['text']
 new_text_data = []
 for text in text_data:
-    #Hapus tag HTML menggunakan HTML parsing
+    #hapus tag HTML menggunakan HTML parsing
     soup = BeautifulSoup(text, 'html.parser')
     newtext = soup.get_text()
     #lower title
@@ -64,11 +64,11 @@ for text in text_data:
     number_text = re.sub(r'\d+', '', lower_title)
     #hapus karakter
     character_text = number_text.translate(str.maketrans('', '', string.punctuation))
-    #Tokenisasi
+    #tokenisasi
     token_text = character_text.split()
     #hapus Stopword
     stopword_text = [word for word in token_text if word not in stop_words]
-    #Lemmatisasi
+    #lemmatisasi
     lematisasi_text = [lemmatizer.lemmatize(stopwords) for stopwords in stopword_text]
     new_text_data.append(lematisasi_text)
 
@@ -82,5 +82,5 @@ data['text'] = new_text_df
 
 concat_df = pd.concat([data['rating'],data['title'],data['text'],data['images'],data['asin'],data['parent_asin'],data['user_id'],data['timestamp'],data['helpful_vote'],data['verified_purchase']],axis=1)
 new_df = pd.DataFrame(concat_df)
-# Menyimpan data dalam format CSV
+# menyimpan data dalam format CSV
 new_df.to_csv(output_file, index=False, encoding='utf-8')
